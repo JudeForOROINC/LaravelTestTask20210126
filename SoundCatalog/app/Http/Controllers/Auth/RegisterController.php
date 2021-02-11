13e0f6bf653b2b28b11f498e8100702f5520e51c
Bob Makhlin <borismakhlin5@gmail.com>
2021-02-11 20:52:53 +0200

    Add captcha to RegisterController

--- SoundCatalog/app/Http/Controllers/Auth/RegisterController.php
+++ SoundCatalog/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Mews\Captcha\Facades\Captcha;
 
 class RegisterController extends Controller
 {
@@ -53,6 +54,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'captcha' => ['required', 'captcha']
         ]);
     }
 
@@ -67,7 +69,15 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'captcha' => $data['captcha'],
             'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    public function refreshCaptcha()
+    {
+        return response()->json([
+            'captcha' => Captcha::img()
         ]);
     }
 }
--- SoundCatalog/captcha/routes/api.php
+++ SoundCatalog/captcha/routes/api.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/refresh-captcha', [\App\Http\Controllers\Auth\RegisterController::class, 'refreshCaptcha'])->name('refreshCaptcha');
