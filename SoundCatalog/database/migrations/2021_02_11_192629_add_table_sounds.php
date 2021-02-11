c26de8ba247b500cc2b1073f39f7bbf8a37a79e8
spetseffect <spetseffect@gmail.com>
2021-02-11 21:38:37 +0200

    add sound migration_new version

--- SoundCatalog/database/migrations/2021_02_11_192629_add_table_sounds.php
+++ SoundCatalog/database/migrations/2021_02_11_192629_add_table_sounds.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSoundsTable extends Migration
+class AddTableSounds extends Migration
 {
     /**
      * Run the migrations.
---
+++ SoundCatalog/resources/views/sounds.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <input type="text" class="form-control" id="search-sound" placeholder="Поиск но названию...">
+        </div>
+        <div class="col-2">
+            <input type="button" class="btn btn-success" id="search-sound-btn" value="Найти">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+
+    </div>
+</div>
+@endsection
